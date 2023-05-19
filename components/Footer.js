f58f46053f4aca93097b20d87bfa0f47ac064285
Diomedes Lajara <diomedes.lajara@gmail.com>
2023-05-19 09:41:48 -0400

    Added footer with links and styles for mobile

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,41 @@
+import { FaMapMarkedAlt } from "react-icons/fa";
+import {
+  AiOutlineMail,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+  AiFillGithub,
+  AiFillCodepenCircle,
+} from "react-icons/ai";
+
 export default function Footer() {
   return (
-    <div className="footer">
-      <center>Footer</center>
-    </div>
+    <footer className="footer">
+      <div className="footNotes">
+        <FaMapMarkedAlt />
+        <p>Fort Lauderdale, Florida</p>
+        <AiOutlineMail />
+        <a href="mailto:webmaster@example.com">diomedes.lajara@gmail.com</a>
+      </div>
+
+      <div className="footSocial">
+        <a href="https://github.com/DlOMEDES">
+          <AiFillGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/diomedeslajara/">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://twitter.com/dl_ajara">
+          <AiFillTwitterCircle />
+        </a>
+        <a href="https://codepen.io/DlOMEDES">
+          <AiFillCodepenCircle />
+        </a>
+      </div>
+
+      <small>
+        Copyright © {2023} Diomedes Lajara, all rights reserved. Designed and
+        Developed by Diomedes{" "}
+      </small>
+    </footer>
   );
 }
